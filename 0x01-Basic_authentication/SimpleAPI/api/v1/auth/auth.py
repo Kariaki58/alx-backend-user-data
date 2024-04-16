15352369146c78b005c8c7e12d0823c9957e813f
kariaki58 <programmerjavascript901@gmail.com>
2024-04-16 11:49:14 +0100

    Auth class

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Auth class """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require authen"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        code_path = path.rstrip('/') + '/'
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                if code_path.startswith(excluded_path.rstrip('*')):
+                    return False
+                else:
+                    pass
+            elif code_path == excluded_path:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """auth header"""
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user"""
+        return None

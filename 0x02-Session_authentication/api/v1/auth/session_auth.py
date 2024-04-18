d981b408e535b5878da2081096b4a586a49573db
kariaki58 <programmerjavascript901@gmail.com>
2024-04-18 14:48:54 +0100

    fixed pycodestyle

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -30,16 +30,14 @@ class SessionAuth(Auth):
         else:
             user_id = SessionAuth.user_id_by_session_id.get(session_id)
             return user_id
-    
+
     def current_user(self, request=None):
         """return if a user session exists"""
         if not request:
             return None
-        
         session_id = self.session_cookie(request)
         if not session_id:
             return None
-        
         user_id = self.user_id_for_session_id(session_id)
         if not user_id:
             return None

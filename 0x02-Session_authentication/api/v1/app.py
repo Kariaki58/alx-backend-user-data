cf563c47fd0bd8de35b4a2691413207e38c22929
kariaki58 <programmerjavascript901@gmail.com>
2024-04-18 14:12:26 +0100

    before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,8 +39,7 @@ def before_request_func():
                              ):
         return
     if auth.authorization_header(request) and auth.session_cookie(request):
-        abort(401)
-        return None
+        return None, abort(401)
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:

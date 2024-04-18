a7f8f93241b318d038e373a608ca297c272a8439
kariaki58 <programmerjavascript901@gmail.com>
2024-04-18 14:43:03 +0100

    testing user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -35,12 +35,12 @@ class SessionAuth(Auth):
         """return if a user session exists"""
         if not request:
             return None
-
+        
         session_id = self.session_cookie(request)
         if not session_id:
             return None
-
-        user_id = self.user_id_for_session_id(session_id)  # Retrieve User ID from session ID
+        
+        user_id = self.user_id_for_session_id(session_id)
         if not user_id:
             return None
 
